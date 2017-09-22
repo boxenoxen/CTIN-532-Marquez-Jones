a30d7779052073c148675ede8c5132f3f828e13d
Temale <temomarquez@Biancas-MacBook-Pro.local>
2017-09-21 19:19:30 -0700

    Edit Sep21,2017
    boundary work
    audio work
    resizing

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    private GameManager gameManager;
+
+	private AudioSource collisionAudioSource;
+
+	public AudioClip collisionAudioClip_Kiss;
+    public AudioClip collisionAudioClip_Honk;
+
+	// Use this for initialization
+	void Start () {
+
+        //GameObject gameManagerObject = new GameObject.FindWithTag("GameManager");
+      
+        //if(gameManagerObject != null){
+            
+        //    gameManager = gameManagerObject.GetComponent<GameManager>();
+        //}
+
+        //if(gameManager == null){
+
+            //Debug.Log("Cannot Find 'Game Manager' script");
+        //}
+	}
+
+    void OnTriggerEnter2D (Collider2D other){
+        if (other.tag == "Left Player"){
+            collisionAudioSource.clip = collisionAudioClip_Kiss;
+            collisionAudioSource.Play();
+            print("We have kissed!");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
