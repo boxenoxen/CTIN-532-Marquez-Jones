0631e57034b011e85802438a92899acd6458d5e3
TemoMarq <cuaotemo.marquez@gmail.com>
2017-09-27 22:04:35 -0700

    UpdateSeptember272017
    Housekeeping and updated scripts.
    
    AudioManager Script has particle systems and emotes.
    
    models have been labeled player one and two.
    
    tags have been implemented.
    
    collision feedbacks have not been implemented.

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,10 +6,15 @@ public class AudioManager : MonoBehaviour {
 
     private GameManager gameManager;
 
-	private AudioSource collisionAudioSource;
+	public AudioSource gameAudioSource;
 
 	public AudioClip collisionAudioClip_Kiss;
     public AudioClip collisionAudioClip_Honk;
+    public AudioClip playerOneEmote;
+    public AudioClip playerTwoEmote;
+
+    public ParticleSystem playerOneParticles;
+    public ParticleSystem playerTwoParticles;
 
 	// Use this for initialization
 	void Start () {
@@ -29,14 +34,43 @@ public class AudioManager : MonoBehaviour {
 
     void OnCollision2D (Collider2D other){
 		if (other.tag == "RP_Head"){
-            collisionAudioSource.clip = collisionAudioClip_Kiss;
-            collisionAudioSource.Play();
+            gameAudioSource.clip = collisionAudioClip_Kiss;
+            gameAudioSource.Play();
             print("We have kissed!");
         }
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PlayerOneEmote();
+
+        }
+  
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            PlayerTwoEmote();
+        }
+    }
+
+    private void PlayerOneEmote()
+    {
+        gameAudioSource.clip = playerOneEmote;
+        gameAudioSource.Play();
+
+        playerOneParticles.Play();
+
+        print("Player One Emotes");
+    }
+
+    private void PlayerTwoEmote ()
+    {
+        gameAudioSource.clip = playerTwoEmote;
+        gameAudioSource.Play();
+
+        playerTwoParticles.Play();
+
+        print("Player Two Emotes");
+    }
 }
