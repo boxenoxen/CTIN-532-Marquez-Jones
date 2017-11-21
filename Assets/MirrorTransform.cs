f174ee9afbba9a941310b22d7d38fda9faeab4ec
Catie Jones <>
2017-11-20 17:10:30 -0800

    11/20/2017 - HEART VFX ALL SCENES

---
+++ Assets/MirrorTransform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorTransform : MonoBehaviour {
+	[SerializeField] Transform _transformToMirror;
+	Vector3 _difference;
+
+	// Use this for initialization
+	void Start () {
+		_difference = _transformToMirror.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = _transformToMirror.position - _difference;
+	}
+}
--- Assets/Scripts/KissCollider.cs
+++ Assets/Scripts/KissCollider.cs
@@ -5,12 +5,15 @@ using System;
 
 public class KissCollider : MonoBehaviour {
     [SerializeField] AudioManager audioManager;
+	[SerializeField] ParticleSystem heart;
 
 	void OnCollisionEnter2D(Collision2D other)
 	{
 		if (other.collider.tag == "PlayerTwo_Head")
 		{
             audioManager.PlayKissSound();
+			heart.Play ();
+
 		}
 	}
 }
