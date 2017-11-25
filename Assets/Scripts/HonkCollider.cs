061d2278e1ca81d6151fe5e75c00db48fcb1b49e
Catie Jones <catiejonesin@gmail.com>
2017-11-25 12:42:00 -0800

    star butts and lots of other things

--- Assets/Scripts/HonkCollider.cs
+++ Assets/Scripts/HonkCollider.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class HonkCollider : MonoBehaviour {
 
 	[SerializeField] AudioManager audioManager;
+	[SerializeField] ParticleSystem star;
 
 	void OnCollisionEnter2D(Collision2D other)
 	{
 		if (other.collider.tag == "PlayerTwo_Pelvis")
 		{
 			audioManager.PlayHonkSound();
+			star.Play ();
+
 		}
 	}
 }
