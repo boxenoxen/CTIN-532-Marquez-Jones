70890d2520e991297d931443fe8cf70260326647
Catie Jones <catiejonesin@gmail.com>
2017-11-26 17:03:26 -0800

    fish

--- Assets/Scripts/FloatEffect.cs
+++ Assets/Scripts/FloatEffect.cs
@@ -12,14 +12,14 @@ public class FloatEffect : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		transform = GetComponent<RectTransform> ();
+		transform = GetComponent<Transform> ();
 		startY = transform.position.y;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		var newY = startY + (startY + Mathf.Cos(Time.time/ duration * 2 )) / .1f;
+		var newY = startY + (startY + Mathf.Cos(Time.time/ duration * 1 )) / .7f;
 		transform.position = new Vector2(transform.position.x, newY);
 
 	}
